8f964e331e7cc18aefb541db3b42aded5bcae163
karlinasuci <karlinasuci026@gmail.com>
2025-01-30 23:05:46 +0700

    test: feat Getting Started and Debugging Tests Using Playwright in Visual Studio Code #1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -17,3 +17,44 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+// test('active and completed filters', async ({ page }) => {
+//   await page.goto('https://demo.playwright.dev/todomvc/#/');
+//   await page.waitForLoadState('domcontentloaded');
+
+//   const input = page.getByPlaceholder('What needs to be done?');
+//   await input.fill('water the plants');
+//   await input.press('Enter');
+//   await page.waitForTimeout(500); 
+
+
+//   await input.fill('feed the dog');
+//   await input.press('Enter');
+//   await page.waitForTimeout(500);
+
+  
+//   await page.locator('li').filter({ hasText: 'water the plants' }).getByRole('checkbox').check();
+
+ 
+//   await page.getByRole('link', { name: 'Active' }).click();
+//   await expect(page.locator('ul.todo-list li')).toContainText('feed the dog');
+
+//   await page.getByRole('link', { name: 'Completed' }).click();
+//   await expect(page.locator('ul.todo-list li')).toContainText('water the plants');
+
+  
+//   await input.press('Enter');
+//   await page.waitForTimeout(500);
+//   await expect(page.locator('ul.todo-list li')).toHaveCount(3);
+
+  
+//   await page.locator('li').filter({ hasText: 'feed the dog' }).getByRole('checkbox').check();
+
+  
+//   await page.getByRole('link', { name: 'Active' }).click();
+//   await expect(page.locator('ul.todo-list li')).toHaveCount(1);
+
+  
+//   await page.getByRole('link', { name: 'Completed' }).click();
+//   await expect(page.locator('ul.todo-list li')).toHaveCount(2);
+// });
