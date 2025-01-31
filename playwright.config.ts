708bf867cb21f34e6acbcc3fb823ca7631ccd072
karlinasuci <karlinasuci026@gmail.com>
2025-01-31 09:25:25 +0700

    test: feat Playwright transpiler configuration. #1

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,9 +12,14 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 30000,
-  globalTimeout: 600000,
-  reporter: 'list',
+  // timeout: 30000,
+  // globalTimeout: 600000,
+  // reporter: 'list',
+
+  build: {
+    external: ['**/*bundle.js'],
+  },
+  
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -25,7 +30,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  //reporter: 'html',
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     // trace: 'on',
